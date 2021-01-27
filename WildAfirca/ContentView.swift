e663ef4e806649f01b2bbd978c4d186d154edd36
Ion Ceban <ceban.ion1992@gmail.com>
2021-01-27 12:51:33 -0600

    Initial Commit

---
+++ WildAfirca/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WildAfirca
+//
+//  Created by Ion Ceban on 1/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WildAfirca/WildAfircaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildAfircaApp.swift
+//  WildAfirca
+//
+//  Created by Ion Ceban on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WildAfircaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
